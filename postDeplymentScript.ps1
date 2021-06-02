092fd0adfb057739c28ef89fe5d2950cf60c58b7
Tariq Alshugran <tariq@prosperoware.com>
2021-06-01 22:23:03 -0400

    update the config function app, add post deployment script to invoke the function automatically

---
+++ postDeplymentScript.ps1
@@ -0,0 +1,7 @@
+param(
+    [string][Parameter(Mandatory=$true)] $configFunctionUrl,
+    [string][Parameter(Mandatory=$true)] $accessKey
+)
+
+$null = az rest --method POST --uri "$($configFunctionUrl)?code=$($accessKey)&clientId=default"
+
