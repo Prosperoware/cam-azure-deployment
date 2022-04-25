9a03db667ee6461b960d278b391ca461bd019db5
Carlos Gutierrez <carlos.gutierrez@prosperoware.co>
2022-04-25 09:52:37 -0500

    REmove code

--- DeployerWizard/awscode/Lambdas/deletesecrets.sh
+++
@@ -1,8 +0,0 @@
- #!/bin/bash
-echo hello worldkch
-output="$(aws secretsmanager list-secrets --profile xher --query 'SecretList[*].ARN' --output text)"
-for i in $output; 
-    do
-        echo $i; 
-        aws secretsmanager delete-secret --secret-id $i --recovery-window-in-days 7 --profile xher
-    done
--- DeployerWizard/awscode/Lambdas/support.sh
+++
@@ -1,4 +0,0 @@
-aws s3 rb s3://prosdevopstest --force --region us-east-1 --profile xher
-aws s3 rm s3://prosdevopstest/devopsinfo.json --region us-east-1 --profile xher
-
-
